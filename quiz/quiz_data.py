913c9548624c3eab9276b8947d3a0160fe3ba5bc
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2024-03-09 20:25:12 +0530

    Add files via upload

---
+++ quiz/quiz_data.py
@@ -0,0 +1,37 @@
+quiz_data =[
+    {
+        "question": "What is the maximum possible length of an identifier",
+        "choices": ["16", "32", "64", "None"],
+        "answer": "None"
+    },
+    {
+        "question": "The output to execute string.ascii_letters can also be obtained from:?",
+        "choices": ["character", "ascii_lowercase_string.digits", "lowercase_string.upercase", "ascii_lowercase+string.ascii_upercase"],
+        "answer": "ascii_lowercase+string.ascii_upercase"
+    },
+    {
+        "question": "Which one of the following is the correct extension of the Python file",
+        "choices": [".py", ".python", ".p", "None"],
+        "answer": ".py"
+    },
+    {
+        "question": "Which of the following declarations is incorrect",
+        "choices": ["_x = 2", "__x = 3", "__xyz__= 5", "None"],
+        "answer": "None"
+    },
+    {
+        "question": "Which of the following precedence order is correct in Python?",
+        "choices": ["Parentheses, Exponential, Multiplication, Division, Addition, Subtraction", "Multiplication, Division, Addition, Subtraction, Parentheses, Exponential","Division, Multiplication, Addition, Subtraction, Parentheses, Exponential","Exponential, Parentheses, Multiplication, Division, Addition, Subtraction"],
+        "answer": "Parentheses, Exponential, Multiplication, Division, Addition, Subtraction"
+        
+    },
+    {
+        "question": "What will be the output of this program?",
+        "choices": ["['XX', 'YY']", "['xx', 'yy']", "[XX, yy]", "None"],
+        "answer": "['XX', 'YY']"
+    }
+]
+    
+    
+
+
